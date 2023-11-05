2f30e64080998def7c50f709cc028402e450602e
fabian1606 <fabian@rafreider.com>
2023-11-05 11:38:44 +0100

    fix bug wrong filename chrashes backend

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
     cb(null, `${appDir}/uploads`);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname.replaceAll(" ", "_"));
   },
 });
 
